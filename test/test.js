96c2ccafde4d3668756a4369f01578515ce45990
1000ch <shogosensui@gmail.com>
2020-05-30 13:25:31 +0900

    Require Node.js 10

--- test/test.js
+++ test/test.js
@@ -12,14 +12,14 @@ const isWebp = require('is-webp');
 const bin = require('..');
 
 test('rebuild the gif2webp binaries', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 
 	await binBuild.url('https://github.com/webmproject/libwebp/archive/v0.6.0.zip', [
-		`mkdir -p ${tmp}`,
-		`make -f makefile.unix examples/gif2webp && mv ./examples/gif2webp ${path.join(tmp, 'gif2webp')}`
+		`mkdir -p ${temporary}`,
+		`make -f makefile.unix examples/gif2webp && mv ./examples/gif2webp ${path.join(temporary, 'gif2webp')}`
 	]);
 
-	t.true(fs.existsSync(path.join(tmp, 'gif2webp')));
+	t.true(fs.existsSync(path.join(temporary, 'gif2webp')));
 });
 
 test('return path to binary and verify that it is working', async t => {
@@ -27,9 +27,9 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('convert a GIF to WebP', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.gif');
-	const dest = path.join(tmp, 'test.webp');
+	const dest = path.join(temporary, 'test.webp');
 	const args = [
 		src,
 		'-o',
@@ -37,8 +37,8 @@ test('convert a GIF to WebP', async t => {
 	];
 
 	await execa(bin, args);
-	const res = await compareSize(src, dest);
+	const result = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
+	t.true(result[dest] < result[src]);
 	t.true(isWebp(await readChunk(dest, 0, 12)));
 });
